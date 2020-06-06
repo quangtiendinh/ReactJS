637d5e98f6cc20436c9fa9fd7300387848ca5c79
quangtiendinh <dinhquangtien0169@gmail.com>
2020-06-07 01:07:32 +0700

    implement social menu

--- myblog/src/shared/components/SocialsMenu/index.js
+++ myblog/src/shared/components/SocialsMenu/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import './index.css';
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {faUser} from "@fortawesome/free-solid-svg-icons";
+import {faFacebook, faFacebookF, faGithub, faSkype, faYoutube} from "@fortawesome/free-brands-svg-icons";
 
 export default class SocialsMenu extends React.Component {
     render() {
@@ -9,17 +10,25 @@ export default class SocialsMenu extends React.Component {
             <div className="socials-menu-component">
                 <ul className="nav justify-content-end">
                     <li className="nav-item">
-                        <a className="nav-link active" href="#">Active</a>
+                        <a className="nav-link facebook" href="#">
+                            <FontAwesomeIcon icon={faFacebookF}/>
+                        </a>
                     </li>
                     <li className="nav-item">
-                        <a className="nav-link" href="#">Link</a>
+                        <a className="nav-link youtube" href="#">
+                            <FontAwesomeIcon icon={faYoutube}/>
+                        </a>
                     </li>
                     <li className="nav-item">
-                        <a className="nav-link" href="#">Link</a>
+                        <a className="nav-link skype" href="#">
+                            <FontAwesomeIcon icon={faSkype}/>
+                        </a>
                     </li>
                     <li className="nav-item">
-                        <a className="nav-link disabled" href="#" tabIndex="-1"
-                           aria-disabled="true">Disabled</a>
+                        <a className="nav-link github" href="#" tabIndex="-1"
+                           aria-disabled="true">
+                            <FontAwesomeIcon icon={faGithub}/>
+                        </a>
                     </li>
                 </ul>
             </div>
