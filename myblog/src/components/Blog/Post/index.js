f0bcb2a0332e7458b59f19e96051a4d52ea7ee4e
Quang Tien Dinh <dinhquangtien0169@gmail.com>
2020-06-06 13:53:45 +0700

    add widget side tab for post page

--- myblog/src/components/Blog/Post/index.js
+++ myblog/src/components/Blog/Post/index.js
@@ -156,6 +156,82 @@ export default class Post extends React.Component {
                     </div>
                     {/*./ widget-courses*/}
 
+                    <div className="sidebar-widget widget-content widget-news">
+                        <div className="widget-header">
+                        <ul className="nav nav-tabs">
+                                <li>
+                                    <a href="#tab-trends" data-toggle="tab" className="active">Xu hướng</a>
+                                </li>
+                                <li>
+                                   <a href="#tab-comments" data-toggle="tab">Bình luận</a> 
+                                </li>
+                                <li>
+                                   <a href="#tab-news-latest" data-toggle="tab">Mới nhất</a> 
+                                </li>
+                            </ul>  
+                        </div>
+                        <div className="widget-body">
+                            <div className="tab-content">
+                                <div id="tab-trends" className="tab-pane in active">
+                                    <div className="media post-item">
+                                        <img src="https://scontent.fsgn5-2.fna.fbcdn.net/v/t1.0-1/c40.0.160.160a/p160x160/50889764_2253631094912976_6090191672140890112_o.jpg?_nc_cat=105&_nc_sid=dbb9e7&_nc_ohc=Iy6XwlnNrH4AX-y3KWE&_nc_ht=scontent.fsgn5-2.fna&oh=a0e0f164c426b4807d34276fb45d7521&oe=5EF9C200" className="mr-3 avatar" alt=""/>
+                                        <div className="media-body">
+                                            <h5 className="post-title">
+                                                Tinohost giảm 30% và nhân đôi thời gian sử dụng hosting khi chuyển dữ liệu từ nơi
+                                            </h5>
+                                        </div>
+                                    </div>
+                                    <div className="media post-item">
+                                        <img src="https://scontent.fsgn5-2.fna.fbcdn.net/v/t1.0-1/c40.0.160.160a/p160x160/50889764_2253631094912976_6090191672140890112_o.jpg?_nc_cat=105&_nc_sid=dbb9e7&_nc_ohc=Iy6XwlnNrH4AX-y3KWE&_nc_ht=scontent.fsgn5-2.fna&oh=a0e0f164c426b4807d34276fb45d7521&oe=5EF9C200" className="mr-3 avatar" alt=""/>
+                                        <div className="media-body">
+                                            <h5 className="post-title">
+                                                Tinohost giảm 30% và nhân đôi thời gian sử dụng hosting khi chuyển dữ liệu từ nơi
+                                            </h5>
+                                        </div>
+                                    </div>
+                                    <div className="media post-item">
+                                        <img src="https://scontent.fsgn5-2.fna.fbcdn.net/v/t1.0-1/c40.0.160.160a/p160x160/50889764_2253631094912976_6090191672140890112_o.jpg?_nc_cat=105&_nc_sid=dbb9e7&_nc_ohc=Iy6XwlnNrH4AX-y3KWE&_nc_ht=scontent.fsgn5-2.fna&oh=a0e0f164c426b4807d34276fb45d7521&oe=5EF9C200" className="mr-3 avatar" alt=""/>
+                                        <div className="media-body">
+                                            <h5 className="post-title">
+                                                Tinohost giảm 30% và nhân đôi thời gian sử dụng hosting khi chuyển dữ liệu từ nơi
+                                            </h5>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div id="tab-comments" className="tab-pane fade">
+                                    <div className="media post-item">
+                                        <img src="https://scontent.fsgn5-2.fna.fbcdn.net/v/t1.0-1/c40.0.160.160a/p160x160/50889764_2253631094912976_6090191672140890112_o.jpg?_nc_cat=105&_nc_sid=dbb9e7&_nc_ohc=Iy6XwlnNrH4AX-y3KWE&_nc_ht=scontent.fsgn5-2.fna&oh=a0e0f164c426b4807d34276fb45d7521&oe=5EF9C200" className="mr-3 avatar" alt=""/>
+                                        <div className="media-body">
+                                            <h5 className="post-title">
+                                                Tinohost giảm 30% và nhân đôi thời gian sử dụng hosting khi chuyển dữ liệu từ nơi
+                                            </h5>
+                                        </div>
+                                    </div>
+                                    <div className="media post-item">
+                                        <img src="https://scontent.fsgn5-2.fna.fbcdn.net/v/t1.0-1/c40.0.160.160a/p160x160/50889764_2253631094912976_6090191672140890112_o.jpg?_nc_cat=105&_nc_sid=dbb9e7&_nc_ohc=Iy6XwlnNrH4AX-y3KWE&_nc_ht=scontent.fsgn5-2.fna&oh=a0e0f164c426b4807d34276fb45d7521&oe=5EF9C200" className="mr-3 avatar" alt=""/>
+                                        <div className="media-body">
+                                            <h5 className="post-title">
+                                                Tinohost giảm 30% và nhân đôi thời gian sử dụng hosting khi chuyển dữ liệu từ nơi
+                                            </h5>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div id="tab-news-latest" className="tab-pane fade">
+                                    <div className="media post-item">
+                                        <img src="https://scontent.fsgn5-2.fna.fbcdn.net/v/t1.0-1/c40.0.160.160a/p160x160/50889764_2253631094912976_6090191672140890112_o.jpg?_nc_cat=105&_nc_sid=dbb9e7&_nc_ohc=Iy6XwlnNrH4AX-y3KWE&_nc_ht=scontent.fsgn5-2.fna&oh=a0e0f164c426b4807d34276fb45d7521&oe=5EF9C200" className="mr-3 avatar" alt=""/>
+                                        <div className="media-body">
+                                            <h5 className="post-title">
+                                                Tinohost giảm 30% và nhân đôi thời gian sử dụng hosting khi chuyển dữ liệu từ nơi
+                                            </h5>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        {/*./ widget-body*/}
+                    </div>
+                    {/*./ widget-courses*/}
+
                 </div>
                 {/*./ widget-news*/}
             </div>
